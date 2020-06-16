096abbc1be896f8e640e0643faa9ed6f6e9638fe
damir <55145337+damirmales@users.noreply.github.com>
2020-06-16 15:10:36 +0200

    update TaskTest delete UserTest

--- tests/AppBundle/Entity/TaskTest.php
+++ tests/AppBundle/Entity/TaskTest.php
@@ -13,30 +13,6 @@ class TaskTest extends TestCase
     $this->task = new Task();
 }
 
-    public function testTitle(){
-
-        $this->task->setTitle("tache");
-        $this->assertEquals($this->task->getTitle(), "tache");
-    }
-
-    public function testContent(){
-
-        $this->task->setContent("tache sachant tacher");
-        $this->assertEquals($this->task->getContent(), "tache sachant tacher");
-    }
-
-    public function testCreatedAt(){
-
-        $this->task->setCreatedAt(2020-05-29);
-        $this->assertEquals($this->task->getCreatedAt(), 2020-05-29);
-    }
-
-    public function testUser(){
-
-        $this->task->setUser("bil");
-        $this->assertEquals($this->task->getUser(), "bil");
-    }
-
     public function testIsDone(){
         $this->task->toggle(true);
         $this->assertEquals($this->task->IsDone(), 1);
